5c902725701c753fbb75440d52d01292841aa3bc
sultanali-mughal <sultanalimughal01@gmail.com>
2023-10-13 23:15:24 +0500

    complete signup page UI

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,6 +3,6 @@ import { signup } from '../controllers/auth.contoller.js';
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post('/signup', signup);
 
 export default router;
