9cbea9a9fc3e4cc3654c113a9d6807560a4e0d22
sultanali-mughal <sultanalimughal01@gmail.com>
2023-10-16 22:51:51 +0500

    complete signIn page functionality

--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -80,7 +80,7 @@ export default function SignUp() {
 
       <div className="flex gap-2 mt-5">
         <p>Have an account</p>
-        <Link to={"/sign-in"}>
+        <Link to={"/signin"}>
           <span className="text-blue-700"> Sign In</span>
         </Link>
       </div>
