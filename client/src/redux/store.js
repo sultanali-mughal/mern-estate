ec48f42d5852b8964db7349a6a98f5fa9ae73752
sultanali-mughal <sultanalimughal01@gmail.com>
2023-10-17 23:08:39 +0500

    syntax correction

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,21 +5,20 @@ import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({ user: userReducer });
 
-
 const persistConfig = {
-    Key: 'root',
-    storage,
-    version: 1,
+  key: 'root',
+  storage,
+  version: 1,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: true,
-        }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
