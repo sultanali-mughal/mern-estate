3a89399eab7320a2a0feefb23cd61d3eba3714fa
sultanali-mughal <sultanalimughal01@gmail.com>
2023-10-25 23:46:36 +0500

    123

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { updateUser } from '../controllers/user.controller.js';
+import { updateUser, deleteUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 //router.get('/test', test);
-router.post('/update/:id', updateUser)
+router.post('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
 
 export default router;
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -81,6 +84,26 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -143,7 +166,7 @@ export default function Profile() {
       </form>
 
       <div className=" flex justify-between mt-5">
-        <span className="text-red-700 curser-pointer">Delete</span>
+        <span onClick={handleDeleteUser} className="text-red-700 curser-pointer">Delete</span>
         <span className="text-red-700 curser-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
